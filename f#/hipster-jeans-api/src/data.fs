8c66d29014fc95d6b710df2ef802d86dfe833183
Pavlos Polianidis <ppoliani@gmail.com>
2017-02-25 23:47:40 +0000

    return random values for all fields except for date

--- f#/hipster-jeans-api/src/data.fs
+++ f#/hipster-jeans-api/src/data.fs
@@ -8,10 +8,14 @@ open Rest
 module Data =
   let countries = [| "Germany"; "UK"; "USA"; "Greece"; "Spain"; "France"; "Italy"; "Netherlands"; "Belgium" |]
   let manufacturers = [| "Denzil Jeans"; "The Hipster Jeans Company"; "Raulph Lauren"; "Tommy Hilfiger"; "Lacoste"; "Gant"; "Armani" |]
-  let Genders= [| "Male"; "Female" |]
+  let genders= [| "Male"; "Female" |]
   let sizes = [| "16"; "32/32"; "34/34"; "32/34"; "18"; "20"; "24"; "36/34"; "36/36"; "38/38" |]
   let colours = [| "red"; "Dark Blue"; "Light Blue"; "Yellow"; "Black"; "white" |]
   let styles = [| "Relaxed"; "Skinny"; "Slim"; "Boot Cut" |]
+  let rnd = System.Random()
+  let getRandomValueFromArray (arr: _[]) =
+    let index = rnd.Next(0, arr.Length - 1)
+    arr.[index]
 
   let getRandomDate =
     let a = new System.DateTime()
@@ -32,13 +36,13 @@ module Data =
 
   let initArray _ =
     { OrderDate=45
-      DeliveryCountry="category 45"
-      Manufacturer="asd"
-      Gender="dfsdf"
-      Size="asdasd"
-      Colour="sdfsf"
-      Style="dfsdf"
-      Count=10UL}
+      DeliveryCountry=getRandomValueFromArray countries
+      Manufacturer=getRandomValueFromArray manufacturers
+      Gender=getRandomValueFromArray genders
+      Size=getRandomValueFromArray sizes
+      Colour=getRandomValueFromArray colours
+      Style=getRandomValueFromArray styles
+      Count=getRandomValueFromArray [|1UL..100UL|]}
 
   let getData =
      JSON (Array.init <| 1000 <| initArray)
