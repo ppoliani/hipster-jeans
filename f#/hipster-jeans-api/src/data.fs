2ba5371d9fc49a5bfbbd6df4e9b8d7a16523226c
Pavlos Polianidis <ppoliani@gmail.com>
2017-02-25 23:33:42 +0000

    return a list of 1000 fixed sales objects

--- f#/hipster-jeans-api/src/data.fs
+++ f#/hipster-jeans-api/src/data.fs
@@ -1,10 +1,24 @@
 namespace Hipster
 
+open System
 open Suave.Swagger
 open Rest
 
 [<AutoOpen>]
 module Data =
+  let countries = [| "Germany"; "UK"; "USA"; "Greece"; "Spain"; "France"; "Italy"; "Netherlands"; "Belgium" |]
+  let manufacturers = [| "Denzil Jeans"; "The Hipster Jeans Company"; "Raulph Lauren"; "Tommy Hilfiger"; "Lacoste"; "Gant"; "Armani" |]
+  let Genders= [| "Male"; "Female" |]
+  let sizes = [| "16"; "32/32"; "34/34"; "32/34"; "18"; "20"; "24"; "36/34"; "36/36"; "38/38" |]
+  let colours = [| "red"; "Dark Blue"; "Light Blue"; "Yellow"; "Black"; "white" |]
+  let styles = [| "Relaxed"; "Skinny"; "Slim"; "Boot Cut" |]
+
+  let getRandomDate =
+    let a = new System.DateTime()
+    let b = new System.DateTime()
+    a
+
+
   [<CLIMutable>]
   type Sale =
     { OrderDate:int
@@ -12,9 +26,19 @@ module Data =
       Manufacturer:string
       Gender:string
       Size:string
-      colour:string
-      style:string
-      count:uint64 }
+      Colour:string
+      Style:string
+      Count:uint64 }
+
+  let initArray _ =
+    { OrderDate=45
+      DeliveryCountry="category 45"
+      Manufacturer="asd"
+      Gender="dfsdf"
+      Size="asdasd"
+      Colour="sdfsf"
+      Style="dfsdf"
+      Count=10UL}
 
   let getData =
-     JSON { Id=45; Name="category 45" }
+     JSON (Array.init <| 1000 <| initArray)
